5576ce67314eccef4cb4905c5334a5c8a6524856
Giliarde Martins <gmartins.mt@gmail.com>
2025-12-06 18:45:12 -0300

    Commit inicial do projeto TodoList

---
+++ src/main/java/com/todolist/TodoList/repository/TodoRepository.java
@@ -0,0 +1,11 @@
+package com.todolist.TodoList.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.todolist.TodoList.entity.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long>{
+	
+	
+
+}
